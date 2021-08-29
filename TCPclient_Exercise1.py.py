dcf2336dd728b54316a73939d26e2fa39caee12e
dj5822 <Dj5822@gmail.com>
2021-08-29 16:15:23 +1200

    Added comments

--- TCPclient_Exercise1.py.py
+++ TCPclient_Exercise1.py.py
@@ -5,9 +5,11 @@ port = 12000
 s = socket.socket()
 
 try:
+    # Connect to the server.
     s.connect((server_ip, port))
     print("Connection established to server {}: {}".format(server_ip, port))
 
+    # Receive data from the server.
     print(s.recv(1024))
 
     s.close()
--- TCPserver_Exercise1.py.py
+++ TCPserver_Exercise1.py.py
@@ -4,6 +4,7 @@ server_ip = 'localhost'
 port = 12000
 s = socket.socket()
 
+# Bind and start the server.
 s.bind((server_ip, port))
 s.listen(5)
 print("Server started listening {}: {}".format(server_ip, port))
@@ -12,6 +13,7 @@ while True:
     c, addr = s.accept()
     print("Got connection from {}".format(addr))
 
+    # Send data to the client.
     c.send('Random message'.encode())
 
     c.close()
